ec656ecaa991ce13788a55419bcd19f042d79217
Nil <nillakhay48@gmail.com>
2025-12-30 11:07:08 +0100

    Some logic Updates.

--- feature-ai/src/main/java/com/petprojject/feature_ai/data/AiGeneratorRepositoryImpl.kt
+++ feature-ai/src/main/java/com/petprojject/feature_ai/data/AiGeneratorRepositoryImpl.kt
@@ -14,45 +14,62 @@ class AiGeneratorRepositoryImpl(
         .generativeModel("gemini-2.5-flash")
 
     override suspend fun getResponseFromAi(request: String): String {
-        return model.generateContent(request).text ?: ""
+        return try {
+            model.generateContent(request).text ?: ""
+        } catch (_: Exception) {
+            ""
+        }
     }
 
     override suspend fun getConclusionAboutUser(): String {
-        val history = carHistoryRepository.getAllCarsHistory()
-        val historyInString = StringBuilder()
-        history.forEach {
-            historyInString.append(it.manufacturer + " " + it.model + " " + it.year)
+        try {
+            val history = carHistoryRepository.getAllCarsHistory()
+            val historyInString = StringBuilder()
+            history.forEach {
+                historyInString.append(it.manufacturer + " " + it.model + " " + it.year)
+            }
+            val prompt =
+                "I'm user.Generate conclusion about me based on my latest search in cars: $historyInString." +
+                        " Answer must be ready to show on android device, that means " +
+                        "that you should not use **,this answer will be used directly in UI."
+            return if (history.size >= MINIMAL_AMOUNT_OF_HISTORY_ITEMS) {
+                model.generateContent(prompt).text ?: ""
+            } else {
+                "You need at least $MINIMAL_AMOUNT_OF_HISTORY_ITEMS items in history"
+            }
+        } catch (_: Exception) {
+            return ""
         }
-        val prompt =
-            "I'm user.Generate conclusion about me based on my latest search in cars: $historyInString." +
-                    " Answer must be ready to show on android device, that means " +
-                    "that you should not use **,this answer will be used directly in UI."
-        return if (history.size >= MINIMAL_AMOUNT_OF_HISTORY_ITEMS) {
-            model.generateContent(prompt).text ?: ""
-        } else {
-            "You need at least $MINIMAL_AMOUNT_OF_HISTORY_ITEMS items in history"
-        }
+
     }
 
     override suspend fun compareCars(
         firstCar: CarHistoryItem,
         secondCar: CarHistoryItem
     ): String {
-        val prompt =
-            "I'm user.Compare 2 cars ${firstCar.manufacturer + " " + firstCar.model + "(" + firstCar.year + ")"} " +
-                    "and ${secondCar.manufacturer + " " + secondCar.model + "(" + secondCar.year + ")"}. " +
-                    "Answer must be ready to show on android device, that means that you should not use **, this answer will be used directly in UI."
-        val response = model.generateContent(prompt).text ?: ""
-        return response
+        try {
+            val prompt =
+                "I'm user.Compare 2 cars ${firstCar.manufacturer + " " + firstCar.model + "(" + firstCar.year + ")"} " +
+                        "and ${secondCar.manufacturer + " " + secondCar.model + "(" + secondCar.year + ")"}. " +
+                        "Answer must be ready to show on android device, that means that you should not use **, this answer will be used directly in UI."
+            val response = model.generateContent(prompt).text ?: ""
+            return response
+        } catch (_: Exception) {
+            return ""
+        }
     }
 
     override suspend fun getCarAlternatives(car: CarHistoryItem): String {
-        val prompt =
-            "I'm user.Give me alternative for car ${car.manufacturer + " " + car.model + "(" + car.year + ")"} " +
-                    "Answer must be ready to show on android device, " +
-                    "that means that you should not use **, this answer will be used directly in UI."
-        val response = model.generateContent(prompt).text ?: ""
-        return response
+        try {
+            val prompt =
+                "I'm user.Give me alternative for car ${car.manufacturer + " " + car.model + "(" + car.year + ")"} " +
+                        "Answer must be ready to show on android device, " +
+                        "that means that you should not use **, this answer will be used directly in UI."
+            val response = model.generateContent(prompt).text ?: ""
+            return response
+        } catch (_: Exception) {
+            return ""
+        }
     }
 
     companion object {
